9ea29d24ef772665b367eb88bf6e2fd51b23d407
imulan <k0223.teru@gmail.com>
2016-12-03 17:31:46 +0900

    Add cc-solution

--- welcome/cpp-correct/main.cc
+++ welcome/cpp-correct/main.cc
@@ -1,18 +1,39 @@
+#include <cstdio>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        int y;
-        cin >> y;
-        int passed = y - 1896;
-        if (passed >= 0 && passed % 4 == 0) {
-            cout << passed / 4 + 1 << endl;
-        } else {
-            cout << -1 << endl;
-        }
-    }
+int solve(int A, int border)
+{
+    if(A>=border) return 0;
+    if(border==100000) return -1;
+    return border+1;
 }
 
+int main() {
+    int CASE;
+    cin >>CASE;
+    while(CASE--)
+    {
+        int N,M,A;
+        cin >>N >>M >>A;
+
+        bool myscore=false;
+        vector<int> S;
+        for(int i=0; i<M; i++)
+        {
+            int t;
+            cin >>t;
+
+            if(!myscore && t==A) myscore=true;
+            else S.push_back(t);
+        }
+
+        sort(S.begin(),S.end(),greater<int>());
+        int border=S[N-1];
+
+        cout << solve(A,border) << endl;
+    }
+    return 0;
+}
